64e9139cc3cd29e863291e4fb8166ba9985e5de3
jalexandersopher <jalexandersopher@gmail.com>
2020-09-12 14:14:12 -0600

    fixed it

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
-var userAge = 17;
-var isSubscribed = true;
+var userAges = [16, 19, 43, 24];
+var subscriptionStatus = [true, false, true, false];
 
-if (userAge >= 18 && isSubscribed) {
-    console.log("User is 18 or older and subscribed");
-} else if (userAge < 18 && isSubscribed) {
-    console.log("User is under 18 and subscribed");
-} else if (userAge >= 18 && !isSubscribed) {
-    console.log("User is 18 or older and is not subscribed");
-} else if (userAge < 18 && !isSubscribed) {
-    console.log("User is under 18 and not subscribed");
-} else {
-    console.log("Something went wrong");
+
+for(var index = 0; index < 4; index++) {
+    var userAge = userAges[index];
+    var isSubscribed = subscriptionStatus[index];
+    
+    if (userAge >= 18 && isSubscribed) {
+        console.log("User is 18 or older and subscribed");
+    } else if (userAge < 18 && isSubscribed) {
+        console.log("User is under 18 and subscribed");
+    } else if (userAge >= 18 && !isSubscribed) {
+        console.log("User is 18 or older and is not subscribed");
+    } else if (userAge < 18 && !isSubscribed) {
+        console.log("User is under 18 and not subscribed");
+    } else {
+        console.log("Something went wrong");
+    }
 }
+
